f00c2788cfe75a2fadd43cf433faeab75deb4f6e
trunglnt <trunglnt@digi-texx.local>
2022-01-18 07:42:56 +0700

    'sign-in'

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import CourseDetail from './Pages/CourseDetail/CourseDetail';
 import CourseList from './Pages/CourseList/CourseList';
 import HomePage from './Pages/Home-page/Home-page';
 import './Sass/main.css';
+import './css/main.css';
 import HomeTemplates from './Templates/HomeTemplates';
+import SignIn from './Pages/User/SignIn';
+import SignUp from './Pages/User/SignUp';
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
         <HomeTemplates exact path="/" Component={HomePage} />
         <HomeTemplates exact path="/khoahoc" Component={CourseList} />
         <HomeTemplates exact path="/chitiet/:courseCode" Component={CourseDetail} />
+        <Route exact path="/dangnhap" component={SignIn} />
+        <Route exact path="/dangky" component={SignUp} />
       </Switch>
     </div>
   );
--- src/Redux/Const/Course-Const.js
+++ src/Redux/Const/Course-Const.js
@@ -2,3 +2,4 @@ export const GET_COURSE_CATALOG = 'GET_COURSE_CATALOG';
 export const GET_COURSE_LIST = 'GET_COURSE_LIST';
 export const SET_KEYCATALOG = 'SET_KEYCATALOG';
 export const GET_COURSE_DETAIL = 'GET_COURSE_DETAIL';
+export const SIGN_IN = 'SIGN_IN';
